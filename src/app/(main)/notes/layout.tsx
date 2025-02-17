84d676f1d654cc2a3bd6a267a348a2092cc9d974
saranatour1 <saranatourcontact@gmail.com>
2025-02-17 19:20:22 +0200

    not important

--- src/app/(main)/notes/layout.tsx
+++ src/app/(main)/notes/layout.tsx
@@ -1,11 +1,11 @@
 import { NotesSideBar } from "@/components/features/notes/SideBar";
 import { ReactNode } from "react";
 
-export default function Layout({children}:{children:ReactNode}){
-  return (<div className="w-full grid grid-cols-[290px_auto] h-full">
-    <NotesSideBar />
-    <div className="w-full h-full flex">
-    {children}
-    </div>
-    </div>)
+export default function Layout({ children }: { children: ReactNode }) {
+	return (
+		<div className="w-full grid grid-cols-[290px_auto] h-full">
+			<NotesSideBar />
+			<div className="w-full h-full flex">{children}</div>
+		</div>
+	);
 }
